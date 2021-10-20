39d86852247f0f025b090c4daddd5466c348a1ca
flamingotiger <hhbhong@gmail.com>
2021-10-20 10:31:15 +0900

    Fix webpack 5 update issue

--- next.config.js
+++ next.config.js
@@ -1,16 +1,17 @@
 const { PHASE_DEVELOPMENT_SERVER, PHASE_PRODUCTION_BUILD } = require('next/constants');
 const dotenv = require('dotenv');
 const withOptimizedImages = require('next-optimized-images');
-const withOffline = require('next-offline');
+// const withOffline = require('next-offline'); // webpack5 호환하지 않음
+// export.modules = withOffline(...);
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
-	enabled: process.env.ANALYZE === 'true'
+	enabled: process.env.ANALYZE === 'true',
 });
 
 dotenv.config();
 
 const { BACKEND_BASE_URL } = process.env;
 
-const nextConfig = phase => {
+const nextConfig = (phase) => {
 	const isDev = phase === PHASE_DEVELOPMENT_SERVER;
 	const isProd = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING !== '1';
 	const isStaging = phase === PHASE_PRODUCTION_BUILD && process.env.STAGING === '1';
@@ -19,20 +20,21 @@ const nextConfig = phase => {
 		BACKEND_BASE_URL: (() => {
 			if (isDev || isProd || isStaging) return BACKEND_BASE_URL;
 			return 'BACKEND_BASE_URL:not (isDev,isProd && !isStaging,isProd && isStaging)';
-		})()
+		})(),
 	};
 	return {
 		typescript: {
-			ignoreDevErrors: true
+			ignoreDevErrors: true,
 		},
 		target: 'serverless',
+		webpack5: true, // webpack5: false면 4버전
 		env,
 		webpack(config, { webpack }) {
 			config.resolve.modules.push(__dirname);
 			config.plugins.push(new webpack.IgnorePlugin(/\/__tests__\//));
 			return config;
-		}
+		},
 	};
 };
 
-module.exports = phase => withOffline(withOptimizedImages(withBundleAnalyzer(nextConfig(phase))));
+module.exports = (phase) => withOptimizedImages(withBundleAnalyzer(nextConfig(phase)));
